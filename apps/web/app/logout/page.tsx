712c5597ae7392a00d302a4505ca508a600b5549
RissRIce <jsdavid278@gmail.com>
2026-07-30 19:52:07 -0600

    fix(auth): handle logout in route handler (#77)

--- apps/web/app/logout/page.tsx
+++
@@ -1,9 +0,0 @@
-import { destroySession } from "@/lib/session";
-import { redirect } from "next/navigation";
-
-export const dynamic = "force-dynamic";
-
-export default async function LogoutPage() {
-  await destroySession();
-  redirect("/");
-}
